88e05884cf54722fd2faf1ac4852ee37486c306d
Guanyu Hu <ikaroinory@gmail.com>
2025-01-01 15:29:37 +0800

    feat: Add ProceedingsBibliography class

---
+++ BibLib.Models/ProceedingsBibliography.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibLib.Models;
+
+[Table("Proceedings")]
+public class ProceedingsBibliography : Bibliography
+{
+    public string? Editor { get; set; }
+    public int? Volume { get; set; }
+    public string? Series { get; set; }
+    public string? Address { get; set; }
+    public string? Organization { get; set; }
+    public string? Publisher { get; set; }
+}
