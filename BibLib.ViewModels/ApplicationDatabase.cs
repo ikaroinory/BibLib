45eca15f15c27a1adf2abdd773c42cb914cf0e96
Guanyu Hu <ikaroinory@gmail.com>
2025-01-01 15:21:26 +0800

    feat: Add database context
    1. Add DbSet<ConferenceBibliography>.
    2. Add DbSet<MastersThesisBibliography>.
    3. DbSet<PhDThesisBibliography>.

--- BibLib.ViewModels/ApplicationDatabase.cs
+++ BibLib.ViewModels/ApplicationDatabase.cs
@@ -7,6 +7,9 @@ public class ApplicationDatabase : DbContext
 {
     public DbSet<ArticleBibliography> Articles { get; set; }
     public DbSet<BookBibliography> Books { get; set; }
+    public DbSet<ConferenceBibliography> Conferences { get; set; }
+    public DbSet<MastersThesisBibliography> MastersTheses { get; set; }
+    public DbSet<PhDThesisBibliography> PhDTheses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
         optionsBuilder.UseSqlite($"Data Source={GlobalResources.DatabaseFilePath}");
