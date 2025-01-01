3d96e4c350ae62f5bb5a6ba6ad1f6a0e09b57715
Guanyu Hu <ikaroinory@gmail.com>
2025-01-01 15:20:37 +0800

    feat: Add PhDThesisBibliography class

---
+++ BibLib.Models/PhDThesisBibliography.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibLib.Models;
+
+[Table("PhDTheses")]
+public class PhDThesisBibliography : Bibliography
+{
+    public string School { get; set; } = string.Empty;
+    
+    public string? Address { get; set; }
+    public string? Keywords { get; set; }
+}
