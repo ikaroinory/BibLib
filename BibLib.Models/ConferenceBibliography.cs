bee14bbd7df086a19bcfc0de719dc007a6bf1ce1
Guanyu Hu <ikaroinory@gmail.com>
2025-01-01 15:15:36 +0800

    feat: Add ConferenceBibliography class

---
+++ BibLib.Models/ConferenceBibliography.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibLib.Models;
+
+[Table("Conferences")]
+public class ConferenceBibliography : Bibliography
+{
+    public string BookTitle { get; set; } = string.Empty;
+
+    public string? Editor { get; set; }
+    public int? Volume { get; set; }
+    public string? Series { get; set; }
+    public string? Pages { get; set; }
+    public string? Address { get; set; }
+    public string? Organization { get; set; }
+    public string? Publisher { get; set; }
+
+    public ConferenceBibliography() { }
+    public ConferenceBibliography(DateTime createTime) : base(createTime) { }
+}
