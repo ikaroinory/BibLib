68fc517e8be6453a211a9cbf80c978e7436fadb0
Guanyu Hu <ikaroinory@gmail.com>
2025-01-01 15:15:52 +0800

    feat: Add MastersThesisBibliography class

---
+++ BibLib.Models/MastersThesisBibliography.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibLib.Models;
+
+[Table("MastersTheses")]
+public class MastersThesisBibliography : Bibliography
+{
+    public string School { get; set; } = string.Empty;
+
+    public string? Type { get; set; }
+    public string? Address { get; set; }
+}
